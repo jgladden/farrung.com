26002496e105d93840db9b453163c7d01a9b8bf0
jgladden <jamesgladden@farrung.com>
2022-04-28 10:20:56 -0700

    prettier eslint husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
